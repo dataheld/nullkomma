f6220d264625b5996a1f71f1af8a3ee6d5441c92
Maximilian Held <info@maxheld.de>
2025-01-29 21:24:11 +0100

    add lock targets

--- make/nix.mk
+++ make/nix.mk
@@ -1,4 +1,6 @@
-.PHONY: check-nix check-flake
+.PHONY: check-nix check-flake update-flake
+
+NIX_FILES := $(wildcard *.nix)
 
 ## Check all nix stuff
 check-nix: check-flake
@@ -6,3 +8,11 @@ check-nix: check-flake
 # Check nix flake
 check-flake:
 	nix flake check --all-systems
+
+# Update flake.lock file
+update-flake:
+	nix flake update
+
+# Complete flake.lock file
+flake.lock: $(NIX_FILES)
+	nix flake lock
