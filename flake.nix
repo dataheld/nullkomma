d77c8b69285cc42c95b0aa253345945a446634ac
Maximilian Held <info@maxheld.de>
2025-02-06 20:52:14 +0100

    format all the things

--- flake.nix
+++ flake.nix
@@ -3,10 +3,12 @@
 
   inputs = {
     nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+    # keep-sorted start
     flake-checker.url = "https://flakehub.com/f/DeterminateSystems/flake-checker/0.2.4.tar.gz";
     flake-iter.url = "https://flakehub.com/f/DeterminateSystems/flake-iter/0.1.92.tar.gz";
     flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/0.1.5.tar.gz";
     format.url = "path:./format";
+    # keep-sorted end
   };
 
   outputs = {
@@ -31,17 +33,21 @@
         });
   in {
     schemas = flake-schemas.schemas;
-    checks = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.runCommand "check" {} ''touch $out'';
-    });
+    checks = forEachSupportedSystem (
+      {pkgs}: {
+        formatting = format.checks.${pkgs.system}.formatting;
+      }
+    );
     devShells = forEachSupportedSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
+          # keep-sorted start
           flake-checker.packages.${pkgs.system}.default
           flake-iter.packages.${pkgs.system}.default
           gnumake
           nixd
           treefmt2
+          # keep-sorted end
         ];
       };
     });
