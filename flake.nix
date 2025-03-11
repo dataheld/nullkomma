0e5944b86206d2a5e034113ce89262d65e074678
Maximilian Held <info@maxheld.de>
2025-03-11 21:05:15 +0100

    try only breaking linux

--- flake.nix
+++ flake.nix
@@ -34,10 +34,7 @@
       ];
       systems = [
         # keep-sorted start
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-darwin"
+        "x86_64-linux"
         # keep-sorted end
       ];
       perSystem =
