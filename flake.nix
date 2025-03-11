51b3e8188dbdfc664f12e734f33aa8bfd1eebcf7
Maximilian Held <info@maxheld.de>
2025-03-11 22:40:53 +0100

    remove linux systems

--- flake.nix
+++ flake.nix
@@ -35,9 +35,7 @@
       systems = [
         # keep-sorted start
         "aarch64-darwin"
-        "aarch64-linux"
         "x86_64-darwin"
-        "x86_64-linux"
         # keep-sorted end
       ];
       perSystem =
