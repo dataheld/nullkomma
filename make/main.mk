6504968e376285de385d51158bf039f52c1f1373
Maximilian Held <info@maxheld.de>
2026-03-25 20:36:24 +0100

    add make file target for gha

--- make/main.mk
+++ make/main.mk
@@ -9,3 +9,10 @@ all: check-flake render
 
 all-check:
 	check-nix
+
+.PHONY: gha
+## Run approximately what is run on GitHub Actions
+gha: gha-ci
+
+.PHONY: gha-ci
+gha-ci: build-nix check-flake-checker
